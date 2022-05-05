40a4b9ea9ed609d0a499a31da0ee4c28d4ca9553
nancyelainej <nancy.e.johnsonn@gmail.com>
2022-05-05 11:22:39 -0400

    new stuff

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
-<?php get_header(); ?>
+<!DOCTYPE html>
+<html lang="en-US">
   <head>
     <title>Home</title>
     <style>
@@ -69,24 +70,6 @@
       }
 
       </style>
+      <?php wp_head(); ?>
   </head>
-  <body>
-  <nav id="NavBar">
-  <ul>
-    <li><a href="index.html">Home</a></li>
-    <li><a href="gallery.html">Gallery</a></li>
-    <li><a href="aboutme.html">About Me</a></li>
-    <li><a href="extrainfo.html">Extra Info</a></li>
-    </ul>
-    </nav>
-    <h1>Hello!</h1>
-    <h1>I Am <i>Nancy</i></h1>
-    <aside>
-    <div class="fakeLink">What do i do?</div>
-    <div class="fakeLink2">Logos</div>
-    <div class="fakeLink3">Websites</div>
-
-
-
-
-<?php get_footer(); ?>
+  <body <?php body_class(); ?>>
